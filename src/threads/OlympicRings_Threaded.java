acef9d36ae1ca965c956089a7f19c4711c0e4c72
Carnecchiad <league@WTS21.attlocal.net>
2017-11-05 12:54:19 -0800

    thread
    threads are threatening

--- src/threads/OlympicRings_Threaded.java
+++ src/threads/OlympicRings_Threaded.java
@@ -1,7 +1,10 @@
 package threads;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
-
+	Robot a = new Robot(0,0);
+	Robot b = new Robot(0,0);
 }
 
